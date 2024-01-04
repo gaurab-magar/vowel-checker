f3c5ad42079dbe101219412c0f98331962bef593
HP <gdikshan10@gmail.com>
2024-01-04 22:40:58 +0545

    endpoint added

--- js/styel.js
+++ js/styel.js
@@ -1,26 +1,16 @@
-let inputValue = document.querySelector('#inputText');
-let btn = document.querySelector('.btn');
-let clear = document.getElementById('clear')
-let result = document.querySelector('#Result');
-btn.addEventListener("click", function(){
-    let text = inputValue.value;
-    let vowelCount = 0;
-    text = text.toLowerCase();
-        for(var i = 0; i < text.length; i++){
-            var char = text.charAt(i);
-            if(isVowel(char)){
-                vowelCount++;
-            }
-        }
-        result.textContent = `RESULT : ${vowelCount}`
+let inputValue = document.getElementById('datetimeInput');
+let outputElement = document.querySelector('.result');
+let button = document.getElementById('Calculate');
+
+button.addEventListener('click',function(){
+    var birthdate = inputValue.value;
+    var today = new Date();
+    var birthDate = new Date(birthdate);
+    var age = today.getFullYear() - birthDate.getFullYear();
+
+    if (today.getMonth() < birthDate.getMonth() || (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
+        age--;
     }
-)
-clear.addEventListener('click',function(){
-    result.textContent=`RESULT : ${0}`;
-    inputValue.value = ''
+    outputElement.innerHTML = `Your Age is ${age}`
 })
 
-function isVowel(char){
-    let vowels = ['a','e','i','o','u'];
-    return vowels.includes(char);
-}
