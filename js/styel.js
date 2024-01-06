9bba844b635020c3a67b76f892eb9219e4bcad35
HP <gdikshan10@gmail.com>
2024-01-06 23:44:29 +0545

    functionality implemnted

--- js/styel.js
+++ js/styel.js
@@ -1,16 +1,19 @@
-let inputValue = document.getElementById('datetimeInput');
-let outputElement = document.querySelector('.result');
-let button = document.getElementById('Calculate');
+let costValue = document.getElementById('cost');
+let tipValue = document.getElementById('tip');
+let calculate = document.querySelector('.btn');
+let result = document.getElementById('result');
 
-button.addEventListener('click',function(){
-    var birthdate = inputValue.value;
-    var today = new Date();
-    var birthDate = new Date(birthdate);
-    var age = today.getFullYear() - birthDate.getFullYear();
-
-    if (today.getMonth() < birthDate.getMonth() || (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-    outputElement.innerHTML = `Your Age is ${age}`
-})
-
+calculate.addEventListener('click',function(){
+    let cost= parseFloat(costValue.value);
+    let tipPercentage=parseFloat(tipValue.value);
+    if(!isFinite(cost) || !isFinite(tipPercentage)){
+        alert("Please enter a valid number");
+        return false;
+        }else{
+            let tipAmount=parseFloat((cost * (tipPercentage/100)).toFixed(2));
+            let totalCost=(cost + tipAmount);
+            //displaying the results
+            result.innerHTML="Tip Amount: $"+tipAmount+"<br> Total Cost"
+            + " including Tip: $"+totalCost;
+            };
+}) 
