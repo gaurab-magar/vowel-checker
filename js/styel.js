afc6001404cfe333fa434a7ab68c5c2d98c76e79
HP <gdikshan10@gmail.com>
2024-01-11 04:31:32 +0545

    functionality added

--- js/styel.js
+++ js/styel.js
@@ -1,23 +1,53 @@
-let inputValue = document.getElementById('taskInput');
-let addButton = document.getElementById('addBtn');
-let listGroup = document.querySelector('.list-group');
+let Button = document.getElementById("item-add");
+let ulElmt = document.querySelector('.list-unstyled');
+let result = document.getElementById('result');
 
-addButton.addEventListener('click',function(){
-    const newTodo = inputValue.value;
-    if(newTodo !== ""){
-        const liItem = document.createElement('li');
-        liItem.classList.add("list-group-item", "border-0" ,"mt-3","fw-bold","d-flex","justify-content-between","align-items-center");
-        liItem.innerText = newTodo;
-        const deleteBtn = document.createElement('button');
-        deleteBtn.classList.add("btn","btn-danger","btn-scale-on-hover");
-        deleteBtn.textContent = "DEL"
-        deleteBtn.addEventListener('click',function(){
-            liItem.remove();
-        })
-        liItem.appendChild(deleteBtn);
-        listGroup.appendChild(liItem);
-        inputValue.value = "";
+let expenses = [];
+let total = 0;
+function renderExpenses(){
+
+    let html = "";
+    expenses.forEach(expense =>{
+        html += `<li class="shadow-lg">
+                    <span class="d-flex justify-content-between align-items-center">
+                        <p>${expense.expenses}</p> <p>$${expense.prices}</p><button class="btn btn-danger fw-bold">Del</button>
+                    </span>
+                </li>`;
+            });
+            ulElmt.innerHTML = html;
+            result.innerText = `Total Expenses: $${total}`;
+}
+function addExpenses(){
+    let description = prompt("Product you wanna bought");
+    let amount = parseFloat(prompt("price of the product"));
+    if(description && amount)
+    {
+        const expense = {
+            expenses :description,
+            prices : amount
+        };
+
+        expenses.push(expense);
+        total += amount;
+        renderExpenses();
     }
-
-
+}
+function deleteExpenses(index){
+    total -= expenses[index].amount;
+    expenses.splice(index,1)
+    renderExpenses();
+}
+ulElmt.addEventListener('click',function(event){
+    if(event.target.classList.contains("btn-danger")){
+        let index = Array.from(event.target.parentNode.parentNode.children)
+        .indexOf(event.target.parentNode);
+        deleteExpenses(index)
+    }
 })
+
+Button.addEventListener('click',function(){
+    addExpenses()
+})
+
+
+
